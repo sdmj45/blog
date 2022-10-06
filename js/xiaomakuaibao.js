a2fb9e66d8133fd346a22c696e94ed59f2918d50
MA Jian <jian.ma-extern@renault.com>
2022-10-06 16:33:51 +0200

    Site updated: 2022-10-06 16:33:48

--- js/xiaomakuaibao.js
+++
@@ -1,7 +0,0 @@
-/*
-ul = $('#post-xiaomakuaibao').find('div.article-entry');
-ul.children('p').each(function(i,li){ul.prepend(li)})*/
-
-var list = $('#post-xiaomakuaibao').find('div.article-entry');
-var listItems = list.children('p');
-list.append(listItems.get().reverse());
